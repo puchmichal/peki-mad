eae0c8a70189a696b81723b85297b578f49d66d5
Patryk <kontakt@patkru.pro>
2018-05-26 15:09:56 +0200

    Encoding correction of poznan.csv

--- datawise_postgres_dumper2.py
+++ datawise_postgres_dumper2.py
@@ -1,2 +1,11 @@
 from sqlalchemy import create_engine
+import numpy as np
+import pandas as pd
+
 engine = create_engine('postgresql://sample_user:!TajemniczaTajemnica7@85.194.245.31:5432/locit_sample')
+
+# STEP 1 - get all the demographic and around-demographic data
+query = 'SELECT * FROM "grid250_demo_ext";'
+demo = pd.read_sql_query(query, con=engine)
+
+print(demo.head())
