107462c1b3438e687b99d8373bc40145a9534537
Patryk <kontakt@patkru.pro>
2018-05-26 15:14:57 +0200

    Dump of demoraphic data into csv file done.

--- datawise_postgres_dumper2.py
+++ datawise_postgres_dumper2.py
@@ -5,7 +5,7 @@ import pandas as pd
 engine = create_engine('postgresql://sample_user:!TajemniczaTajemnica7@85.194.245.31:5432/locit_sample')
 
 # STEP 1 - get all the demographic and around-demographic data
-query = 'SELECT * FROM "grid250_demo_ext";'
+query = 'SELECT * FROM locit_datasets.grid250_demo_ext;'
 demo = pd.read_sql_query(query, con=engine)
 
-print(demo.head())
+demo.to_csv(path_or_buf="data_demo", sep=";")
