da351f1c59b53474008a97deae167ee99014872a
Agata Lonc <alonc96@gmail.com>
2018-05-27 02:16:54 +0200

    cleaning repo

--- csv_read_pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import geopandas as gpd
-from shapely.geometry import Point
-
-przedszkola = pd.read_csv('dane.csv')
-wsp = przedszkola.apply(lambda z: Point(z.X, z.Y), axis=1)
-crs = {'init': 'epsg:2263'}
-przedszkola = gpd.GeoDataFrame(przedszkola)
